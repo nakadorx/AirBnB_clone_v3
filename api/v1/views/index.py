888543c2bf4c27d4e2ad11ec0c5cfbdcde927825
nakadorx <nakadorx@gmail.com>
2020-09-10 00:22:50 +0100

    kol

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,12 +5,7 @@ fahras
 from api.v1.views import app_views
 from flask import jsonify
 from models import storage
-from models.amenity import Amenity
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
-from models.user import User
+
 
 
 @app_views.route('/status')
@@ -26,12 +21,9 @@ def counts():
     """
     hamammet hammamet
     """
-    result = {
-            "amenities": storage.count(Amenity),
-            "cities": storage.count(City),
-            "places": storage.count(Place),
-            "reviews": storage.count(Review),
-            "states": storage.count(State),
-            "users": storage.count(User)
-            }
-    return jsonify(result)
+    return jsonify({"amenities": storage.count('Amenity'),
+                    "cities": storage.count('City'),
+                    "places": storage.count('Place'),
+                    "reviews": storage.count('Review'),
+                    "states": storage.count('State'),
+                    "users": storage.count('User')})
