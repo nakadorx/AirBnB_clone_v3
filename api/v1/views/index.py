6b71e9697b56bca51c4678d85bcf7b0e33440284
nakadorx <nakadorx@gmail.com>
2020-09-10 00:06:58 +0100

    kol

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -6,11 +6,11 @@ from api.v1.views import app_views
 from flask import jsonify
 from models import storage
 from models.amenity import Amenity
+from models.state import State
 from models.city import City
 from models.place import Place
-from models.review import Review
-from models.states import State
 from models.user import User
+from models.review import Review
 
 
 @app_views.route('/status')
