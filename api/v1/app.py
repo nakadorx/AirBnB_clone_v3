fa2e1e665a0daac4b9df466d40e0f748be57523f
medcharfi96 <1389@holbertonschool.com>
2020-09-10 18:44:24 +0200

    add

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,10 +5,12 @@ app file and fuction
 
 from api.v1.views import app_views
 from flask import Flask, render_template, jsonify, Response
+from flask_cors import CORS
 from models import storage
 from os import getenv
 app = Flask(__name__)
 app.register_blueprint(app_views)
+cs = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
